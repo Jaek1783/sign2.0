ea756d206379113f02959eca216fe4bf8056dbdc
jaekeun Choi <jaek1783@gmail.com>
2022-09-23 19:37:58 +0900

    login, 회원가입, input값 ref.current.value로 넣어 새로운 배열 만들기

--- src/LoadingPage/SignUp.js
+++ src/LoadingPage/SignUp.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React,{useRef, useState} from 'react';
 
-function SignUp(props) {
+const SignUp = () => {
+    const nameRef = useRef(null);
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const user = [];
+const sign = ()=>{
+    user.push({
+        name:nameRef.current.value,
+        email:emailRef.current.value,
+        password:passwordRef.current.value
+    });
+    console.log(user);
+    nameRef.current.value = "";
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+}
     return (
         <div className='card' style={{width: 18 + 'rem', padding: 1 + 'rem', margin:`1rem auto`}}>
             <h2>Sign Up</h2>
             <div className="mb-3">
                 <label htmlFor="exampleFormControlInput1" className="form-label">Name</label>
-                <input type="email" className="form-control" placeholder="Your Name"/>
+                <input type="email" className="form-control" placeholder="Your Name" ref={nameRef}/>
             </div>
             <div className="mb-3">
                 <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                <input type="email" className="form-control" placeholder="name@example.com"/>
+                <input type="email" className="form-control" placeholder="name@example.com" ref={emailRef}/>
             </div>
             <div className="mb-3">
                 <label htmlFor="exampleFormControlInput1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleFormControlInput1"/>
+                <input type="password" className="form-control" id="exampleFormControlInput1"  ref={passwordRef}/>
             </div>
-            <button type="button" className="btn btn-success">Sign Up</button>
+            <button type="button" className="btn btn-success" onClick={()=>{
+                sign();
+            }}>Sign Up</button>
         </div>
     );
 }
